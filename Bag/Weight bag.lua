a7e9649b6f8ec7f23ca9096e0ec20d58cd75edc4
Bornold <logoron12345@gmail.com>
2021-02-16 21:55:21 +0800

    Initial commit

---
+++ Bag/Weight bag.lua
@@ -0,0 +1,45 @@
+function onObjectHover()
+  currentWeight = 0
+  for _,v in pairs(self.getObjects()) do
+    WeightCalculation(v.description)
+  end
+  Wait.frames(|| SetNumber(), 5)
+end
+
+function WeightCalculation(allDesctiption, num)
+  local isFlag = false
+  for S in allDesctiption:gmatch("%S+") do
+    if(isFlag) then
+      S = S:gsub(",", ".")
+      currentWeight = currentWeight + tonumber(S)
+      break
+    end
+    if(S == "вес:" or S == "weight:") then
+      isFlag = true
+    end
+  end
+  if(currentWeight < 0) then currentWeight = 0 end
+  SetNumber()
+end
+
+function SetNumber()
+  local prevDescription = self.getDescription()
+  local isFlag = false
+  if(prevDescription:find("weight:")) then
+    for S in prevDescription:gmatch("%S+") do
+      if(isFlag) then
+        local findNumber = prevDescription:find(S)
+        local startText = prevDescription:sub(1, findNumber - 1)
+        local endText = prevDescription:sub(#startText + findNumber + 1)
+        startText = startText .. currentWeight .. endText
+        self.setDescription(startText)
+        break
+      end
+      if(S == "weight:") then
+        isFlag = true
+      end
+    end
+  else
+	  self.setDescription(prevDescription .. "/nweight: " .. currentWeight)
+  end
+end
---
+++ Plate/Weight plate.lua
@@ -0,0 +1,83 @@
+function UpdateSave()
+  local dataToSave = { ["maxWeight"] = maxWeight,
+    ["allObjectGUID"] = allObjectGUID}
+  local savedData = JSON.encode(dataToSave)
+  self.script_state = savedData
+end
+
+function onLoad(savedData)
+  allObjectGUID = {}
+  currentWeight, maxWeight = 0, 20
+  if(savedData != "") then
+    local loadedData = JSON.decode(savedData)
+    maxWeight = loadedData.maxWeight or 20
+    allObjectGUID = loadedData.allObjectGUID or {}
+  end
+  Wait.Frames(SetNumber, 5)
+end
+
+function onObjectHover()
+  WeightCalculation()
+end
+
+function SetNumber()
+  local textWeight = "Weight: " .. currentWeight .. "/" .. maxWeight
+	self.UI.setValue("weight", textWeight)
+  UpdateSave()
+end
+
+function InputChangeWeight(player, input)
+  maxWeight = tonumber(input ~= "" and input or "0")
+  local textWeight = "Weight: " .. currentWeight .. "/" .. maxWeight
+	self.UI.setValue("weight", textWeight)
+  UpdateSave()
+end
+
+function ReturnOriginal()
+	self.UI.setAttribute("inputMaxWeight", "text", 99)
+end
+
+function onCollisionEnter(obj)
+  if(currentWeight) then
+    table.insert(allObjectGUID, obj.collision_object.getGUID())
+    WeightCalculation()
+  end
+end
+
+function onCollisionExit(obj)
+  if(currentWeight) then
+    local locGUID = obj.collision_object.getGUID()
+    for i,v in ipairs(allObjectGUID) do
+      if(v == locGUID) then
+        table.remove(allObjectGUID, i)
+        break
+      end
+    end
+    WeightCalculation()
+  end
+end
+
+function WeightCalculation()
+  currentWeight = 0
+  for i,v in ipairs(allObjectGUID) do
+    if(not getObjectFromGUID(v)) then table.remove(allObjectGUID, i) break end
+    local allDesctiption = getObjectFromGUID(v).getDescription()
+    local isFlag = false
+    for S in allDesctiption:gmatch("%S+") do
+      if(isFlag) then
+        S = S:gsub(",", ".")
+        currentWeight = currentWeight + tonumber(S)
+        break
+      end
+      if(S == "вес:" or S == "weight:") then
+        isFlag = true
+      end
+    end
+  end
+  SetNumber()
+end
+
+function Reset()
+	currentWeight = 0
+  SetNumber()
+end
